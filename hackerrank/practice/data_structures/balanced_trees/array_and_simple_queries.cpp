41a56f1b778e33e4ec4293fe06ea8ba2ca8575a9
Loks- <alexeypoyarkov@gmail.com>
2025-08-16 22:13:42 -0700

    Sync with new function name.

--- hackerrank/practice/data_structures/balanced_trees/array_and_simple_queries.cpp
+++ hackerrank/practice/data_structures/balanced_trees/array_and_simple_queries.cpp
@@ -19,10 +19,11 @@ int main_array_and_simple_queries() {
   for (; M; --M) {
     unsigned t, i, j;
     cin >> t >> i >> j;
-    if (t == 1)
-      bst::SwapIntervals<TTree>(root, 0, i - 1, j);
-    else
-      bst::SwapIntervals<TTree>(root, i - 1, j, N);
+    if (t == 1) {
+      bst::utils::swap_intervals<TTree>(root, 0, i - 1, j);
+    } else {
+      bst::utils::swap_intervals<TTree>(root, i - 1, j, N);
+    }
   }
   vector<int> vf = bst::base::to_vector(root);
   cout << abs(vf[0] - vf.back()) << endl;
